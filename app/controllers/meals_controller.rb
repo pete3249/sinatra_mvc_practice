9d0fc100fb405456e575b89814afa6a9d1c15117
pete3249 <katelynrpeterson16@gmail.com>
2020-08-25 00:00:00 -0400

    Updated Post: /meals

--- app/controllers/meals_controller.rb
+++ app/controllers/meals_controller.rb
@@ -14,8 +14,11 @@ class MealsController < ApplicationController
 
   # POST: /meals
   post "/meals" do
-    binding.pry
-    redirect "/meals"
+    #assign new object to variable
+    meal = Meal.create(meal_params)
+    #insert happens with .create and params end up in a row in a database
+    #redirect always sends a GET request
+    redirect "/meals/#{meal.id}"
   end
 
   # GET: /meals/5 (SHOWs details about a particular meal)
@@ -39,4 +42,12 @@ class MealsController < ApplicationController
   delete "/meals/:id" do
     redirect "/meals"
   end
+
+  private
+
+  def meal_params
+    allowed = ["name", "price", "spicyness"]
+    params.select{|key| allowed.include?(key)}
+  end 
+
 end
