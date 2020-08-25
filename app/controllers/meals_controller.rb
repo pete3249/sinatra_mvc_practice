273df255556b9c31bf9c59844d9eaf51762f9f0c
pete3249 <katelynrpeterson16@gmail.com>
2020-08-24 23:19:50 -0400

    Updated New and Show erb

--- app/controllers/meals_controller.rb
+++ app/controllers/meals_controller.rb
@@ -14,11 +14,14 @@ class MealsController < ApplicationController
 
   # POST: /meals
   post "/meals" do
+    binding.pry
     redirect "/meals"
   end
 
-  # GET: /meals/5 (details about a particular meal)
+  # GET: /meals/5 (SHOWs details about a particular meal)
   get "/meals/:id" do
+    @meal = Meal.find(params[:id])
+    #file below is an argument interpretted by erb #self.erb :"/meals/show.html"
     erb :"/meals/show.html"
   end
 
@@ -27,13 +30,13 @@ class MealsController < ApplicationController
     erb :"/meals/edit.html"
   end
 
-  # PATCH: /meals/5
+  # PATCH: /meals/5 #for update
   patch "/meals/:id" do
     redirect "/meals/:id"
   end
 
-  # DELETE: /meals/5/delete
-  delete "/meals/:id/delete" do
+  # DELETE: /meals/5
+  delete "/meals/:id" do
     redirect "/meals"
   end
 end
